d4936a8ea06ed2f4b984c9311f27043f20baa523
jeremyestrada <jeremy.estrada@eupraxialabs.com>
2022-04-07 18:00:02 -0500

    Full-stack Development Updated. Redefined Activity Pages implemented. Face Recog camera compatibility.

--- src/back-end/server.py
+++ src/back-end/server.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, url_for , Response
+from flask import Flask, render_template, url_for, Response
 from flask_bootstrap import Bootstrap
 from Operations import runBlankCamera, runMotionDetectionCam
-from FaceRecog import FaceRecogOps,face_train
-
+from FaceRecog import FaceRecogOps, face_train
 
 from Camera import BlankCamera, FaceRecognitionCam, MotionDetectionCam
 from MotionDetection import MDOps
@@ -11,13 +10,10 @@ import os
 
 from datetime import datetime
 
-
-
 # MODE = os.getenv('FLASK_ENV')
 # DEV_SERVER_URL = 'http://localhost:3000/'
 
-
-#TO DO: FIND CLIENT FOLDER WITHOUT HARD CODING THE PATH.
+# TO DO: FIND CLIENT FOLDER WITHOUT HARD CODING THE PATH.
 # templateDir = os.path.abspath("/home/thanh/Desktop/computer_vision/project_workspace/src/front-end/src")
 # templateDir = os.path.abspath("/home/thanh/Desktop/computer_vision/project_workspace/src/back-end/client")
 
@@ -32,12 +28,12 @@ def gen(camera):
     while True:
         frame = camera.Capture()
         frameName = "cam.jpg"
-        cv2.imwrite(frameName,frame)
-        frame = open(frameName,'rb').read()
+        cv2.imwrite(frameName, frame)
+        frame = open(frameName, 'rb').read()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-def gen2(camera):   
+def gen2(camera):
     detector = MDOps.MDOps()
     while True:
         frame = camera.Capture()
@@ -45,16 +41,18 @@ def gen2(camera):
 
         font = cv2.FONT_HERSHEY_SIMPLEX
         now = datetime.now()
-        cv2.putText(frame,str(now.replace(microsecond=0)),(10,30), font, 1,(255,255,255),2,cv2.LINE_AA)
+        cv2.putText(frame, str(now.replace(microsecond=0)), (10, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         frameName = "Motion.jpg"
-        cv2.imwrite(frameName,frame)
-        frame = open(frameName,'rb').read()
+        cv2.imwrite(frameName, frame)
+        frame = open(frameName, 'rb').read()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
         # frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+
+
 def gen3(camera):
-    camera = FaceRecognitionCam.FaceRecognitionCam(0,"Door Camera")
+    camera = FaceRecognitionCam.FaceRecognitionCam(0, "Door Camera")
     if not os.path.isfile("FaceRecog/labels.pkl"):
         face_train.train()
     detector = FaceRecogOps.FaceRecogOps()
@@ -62,10 +60,9 @@ def gen3(camera):
         frame = camera.Capture()
         frame = detector.recogLBHP(frame)
         frameName = "Face.jpg"
-        cv2.imwrte(frameName,frame)
-        frame = open(frameName,'rb').read()
+        cv2.imwrite(frameName, frame)
+        frame = open(frameName, 'rb').read()
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
         # cv2.imshow("imgage",res)
         # if cv2.waitKey(1) == 32:
@@ -78,13 +75,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/video_feed',methods=['GET'])
+@app.route('/2')
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/statistics')
+def statistics():
+    return render_template('stats.html')
+
+
+# For Camera 1
+@app.route('/video_feed', methods=['GET'])
 def video_feed():
-    camera = MotionDetectionCam.MotionDetectionCam(0,"Porch Camera")
+    camera = MotionDetectionCam.MotionDetectionCam(0, "Porch Camera")
     return Response(gen2(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     # camera = BlankCamera.BlankCamera(0,"Blank Camera")
     # return Response(gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# For Camera 2
+@app.route('/video_feed2', methods=['GET'])
+def video_feed2():
+    camera = FaceRecognitionCam.FaceRecognitionCam(0, "Door Camera")
+    return Response(gen3(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
