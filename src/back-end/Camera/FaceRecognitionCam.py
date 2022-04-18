b9ccc0ac7b99fce893411658f9346fb82bf98693
Julian <60678586+thanhvu3105@users.noreply.github.com>
2022-04-17 20:57:16 -0500

    Update FaceRecognitionCam.py

--- src/back-end/Camera/FaceRecognitionCam.py
+++ src/back-end/Camera/FaceRecognitionCam.py
@@ -1,6 +1,7 @@
 from Camera import ICamera
 import cv2
 
+#Facial Recognition camera
 class FaceRecognitionCam(ICamera.ICamera):
     def __init__(self,index,cameraName):
         super().__init__(index,cameraName)
